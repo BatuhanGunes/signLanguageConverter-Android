548fc7cecdc1849b7acb967a3b683dd98a2cd144
Batuhan Güneş <batuhangunes001@gmail.com>
2020-05-20 01:14:58 +0300

    add splash activity screen file

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/classification/myappview/SplashActivity.java
@@ -0,0 +1,16 @@
+package org.tensorflow.lite.examples.classification.myappview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.tensorflow.lite.examples.classification.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
