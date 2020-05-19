b3ba50ff846e27fed45d7ca3f404cfb22b020070
Batuhan Güneş <batuhangunes001@gmail.com>
2020-05-20 01:59:00 +0300

    add splash screen code
    The splash screen is set to disappear for 1 second. Then followed by MainActivity

--- android/app/src/main/java/org/tensorflow/lite/examples/classification/myappview/SplashActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/classification/myappview/SplashActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.tensorflow.lite.examples.classification.MainActivity;
 import org.tensorflow.lite.examples.classification.R;
+
+import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +15,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
