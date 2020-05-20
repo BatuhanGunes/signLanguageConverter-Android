121ae1f274b187060d0254926181ab09e33d4a62
Batuhan Güneş <batuhangunes001@gmail.com>
2020-05-20 11:34:51 +0300

    Create AboutUsActivity files
    Create .java and .xml file. Definition abou_us_activitiy  in android manifest

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/classification/myappview/AboutUsActivity.java
@@ -0,0 +1,16 @@
+package org.tensorflow.lite.examples.classification.myappview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.tensorflow.lite.examples.classification.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
