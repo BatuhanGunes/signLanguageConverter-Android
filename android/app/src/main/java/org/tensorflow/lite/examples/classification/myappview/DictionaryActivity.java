b83cdaed8c2b8d772d5d42bde6501cdd742db5e4
Batuhan Güneş <batuhangunes001@gmail.com>
2020-05-20 21:31:00 +0300

    Create Dictionary_Activity screen files

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/classification/myappview/DictionaryActivity.java
@@ -0,0 +1,16 @@
+package org.tensorflow.lite.examples.classification.myappview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.tensorflow.lite.examples.classification.R;
+
+public class DictionaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+    }
+}
